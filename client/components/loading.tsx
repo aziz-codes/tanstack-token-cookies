2a855c807011407ca5521fc3831db7c194d07c23
aziz-codes <azizisoffline@gmail.com>
2024-08-28 01:46:55 +0500

    laoding component

---
+++ client/components/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingButton = () => {
+  return (
+     
+        <div className='h-8 w-8 rounded-full border border-t-2 border-t-sky-400 animate-spin mx-auto'></div>
+    
+  )
+}
+
+export default LoadingButton
