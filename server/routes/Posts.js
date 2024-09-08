ee2d7a61b6f793fcfe73eca22c53bc989398da36
aziz-codes <azizisoffline@gmail.com>
2024-09-08 23:24:34 +0500

    hanlder changed

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getPosts, savePosts,likePost} from "../controllers/Post.js";
+import { getPosts, savePosts,handleReactPost} from "../controllers/Post.js";
 const router = express();
 
 router.get("/", getPosts);
 router.post("/", savePosts);
 
-router.put("/:id/like", likePost); 
+router.put("/:id/like", handleReactPost); 
 
 export default router;
