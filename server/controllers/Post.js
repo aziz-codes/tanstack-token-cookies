1cf33c785d7432533e038d759b9b21769454e035
aziz-codes <azizisoffline@gmail.com>
2024-09-08 23:24:55 +0500

    controller issue

--- server/controllers/Post.js
+++ server/controllers/Post.js
@@ -1,4 +1,5 @@
 import Post from "../schema/Post.Schema.js";
+
 export const getPosts = async (_, res) => {
   try {
     const posts = await Post.find().sort({ createdAt: -1 });
@@ -29,5 +30,39 @@ export const savePosts = async (req, res) => {
     res
       .status(500)
       .json({ message: "Server erorr", success: false, error: err.message });
+  }
+};
+
+
+export const handleReactPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { user } = req.body;  
+    console.log(user)
+    const post = await Post.findById(id);  
+    if (!post) {
+      return res.status(404).json({ message: "Post not found", id });
+    }
+
+    const hasLiked = post.likes.includes(user);  
+
+    if (hasLiked) {
+     const updatedLikes =  post.likes.filter((like) => like !== user);
+       post.likes = updatedLikes;
+       
+    } else {
+ 
+      post.likes.push(user);
+    }
+    await post.save(); 
+
+    // Send the correct response after saving
+    res.status(200).json({
+      message: hasLiked ? "Like removed" : "Like added",
+      post,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
